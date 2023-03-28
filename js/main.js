4450e87f67dc0558e5652298a86d338be4036066
bee95 <dcb95@naver.com>
2023-03-28 18:28:37 +0900

    자바스크립트

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const img = document.querySelector(".img");
+/*const img = document.querySelector(".img");
 const profile1 = document.querySelector(".profile1");
 const profile2 = document.querySelector(".profile2");
 const profile3 = document.querySelector(".profile3");
@@ -14,4 +14,15 @@ profile2.addEventListener("click", (e) => {
 profile3.addEventListener("click", (e) => {
   let path = e.currentTarget.getAttribute("src");
   img.setAttribute("src", path);
+});*/
+
+const big = document.querySelector(".bigimgaes");
+const small = document.querySelector(".smallimges ");
+for (let i = 0; i < smallimges.length; i++) {
+  small[i].addEventListener("click", changepic);
+}
+
+small.addEventListener("click", (e) => {
+  let changepic = e.currentTarget.getAttribute("src");
+  big.setAttribute("src", changepic);
 });
