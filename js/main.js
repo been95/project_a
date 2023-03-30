2047607963b6cde358db7c87823f9de6544db256
염채빈 <yeomchaebin@yeomchaebin-ui-MacBookPro.local>
2023-03-30 21:49:57 +0900

    프로필부분 스크립트 완성

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/*const img = document.querySelector(".img");
+const img = document.querySelector(".img");
+
+/*
 const profile1 = document.querySelector(".profile1");
 const profile2 = document.querySelector(".profile2");
 const profile3 = document.querySelector(".profile3");
@@ -14,15 +16,54 @@ profile2.addEventListener("click", (e) => {
 profile3.addEventListener("click", (e) => {
   let path = e.currentTarget.getAttribute("src");
   img.setAttribute("src", path);
-});*/
+});
+*/
 
 const big = document.querySelector(".bigimgaes");
-const small = document.querySelector(".smallimges ");
-for (let i = 0; i < smallimges.length; i++) {
-  small[i].addEventListener("click", changepic);
-}
+const thumbs = document.querySelectorAll(".thumbs");
 
-small.addEventListener("click", (e) => {
-  let changepic = e.currentTarget.getAttribute("src");
-  big.setAttribute("src", changepic);
+console.log(thumbs);
+
+thumbs.forEach( (e,i) => {
+  e.addEventListener('click', (event) => {
+    big.setAttribute('src','./img/img' + (i+1) + '.png');
+  })
+})
+console.log('hello');
+
+/*
+thumbs.forEach( (e, i, arrys) => {
+  console.log(`e : ${e}, i : ${i}, arrys : ${arrys}`)
+  big.setAttribute('src','./img/img' + (i+1) + '.png');
+})
+*/
+ 
+console.log('world')
+ 
+/*
+for (let i = 0; i < thumbs.length; i++) {
+  thumbs[i].addEventListener("click", (e) => {
+    big.setAttribute('src','./img/img' + (i+1) + '.png');
+  });
+}
+*/
+ 
+
+/*
+let path = './img/';
+ 
+
+thumbs[0].addEventListener("click", (e) => {
+ 
+  big.setAttribute("src", path + 'img1.png');
 });
+thumbs[1].addEventListener("click", (e) => { 
+  big.setAttribute("src", path + 'img2.png');
+});
+thumbs[2].addEventListener("click", (e) => { 
+  big.setAttribute("src", path + 'img3.png');
+});
+
+*/
+
+  
