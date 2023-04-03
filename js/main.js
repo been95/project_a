6f21ba1a12518ab3e42ab05123da095f24b53055
염채빈 <yeomchaebin@yeomchaebin-ui-MacBookPro.local>
2023-04-04 08:39:56 +0900

    feat: add

--- js/main.js
+++ js/main.js
@@ -23,9 +23,46 @@ const big = document.querySelector(".bigimgaes");
 const thumbs = document.querySelectorAll(".thumbs");
 const modalcontainer = document.querySelector(".modal-container");
 const dim = document.querySelector(".dim");
-const thumbnail = document.querySelector(".thumbnail");
+const thumbnail = document.querySelectorAll(".thumbnail");
 const closeBtn = document.querySelector(".btn-close");
-console.log(thumbs);
+const datas = [
+  {
+    id : 0,
+    img : "./img/g1.jpg",
+    alt : "베이킹 히포",
+    title : "폴라로이드",
+    date : "2022.03.04",
+    type : "디지털드로잉",
+    tool : "아이패드 프로크리에이트",
+    memo : "따사로운 햇살과 친구들과 함께 공원에서 놀고 있는 히포와 츄덕이",
+    photo1 : "./img/g1.jpg",
+    photo2 : "./img/g2.jpg",
+  },
+  {
+    id : 1,
+    img : "./img/g2.jpg",
+    alt : "베이킹 히포",
+    title : "폴라로이드",
+    date : "2022.03.04",
+    type : "디지털드로잉",
+    tool : "아이패드 프로크리에이트",
+    memo : "따사로운 햇살과 친구들과 함께 공원에서 놀고 있는 히포와 츄덕이",
+    photo1 : "./img/g1.jpg",
+    photo2 : "./img/g2.jpg",
+  },
+  {
+    id : 2,
+    img : "./img/g3.jpg",
+    alt : "베이킹 히포",
+    title : "폴라로이드",
+    date : "2022.03.04",
+    type : "디지털드로잉",
+    tool : "아이패드 프로크리에이트",
+    memo : "따사로운 햇살과 친구들과 함께 공원에서 놀고 있는 히포와 츄덕이",
+    photo1 : "./img/g1.jpg",
+    photo2 : "./img/g2.jpg",
+  }
+]
 
 thumbs.forEach((e, i) => {
   e.addEventListener("click", (event) => {
@@ -40,6 +77,8 @@ thumbs.forEach( (e, i, arrys) => {
   big.setAttribute('src','./img/img' + (i+1) + '.png');
 })
 */
+
+
 modalcontainer.addEventListener("click", (e) => {
   if (modalcontainer.classList.contains("active")) {
     modalcontainer.classList.remove("active");
@@ -48,20 +87,23 @@ modalcontainer.addEventListener("click", (e) => {
   }
 });
 
-thumbnail.addEventListener("click", (e) => {
-  modalcontainer.style.right = 0;
-  closeBtn.style.display = "flex";
-  dim.style.display = "block";
+thumbnail.forEach((e,i)=>{
+e.addEventListener("click",(event)=>{
+  event.preventDefault(0); 
+   modalcontainer.style.left= 0;
+   closeBtn.style.display = "block";
+   dim.style.display = "block";
+  });
 });
 
 closeBtn.addEventListener("click", (e) => {
-  modalcontainer.style.right = "-75%";
+  modalcontainer.style.left = "-600px";
   closeBtn.style.display = "none";
   dim.style.display = "none";
 });
 
 dim.addEventListener("click", (e) => {
-  modalcontainer.style.right = "-90%";
+  modalcontainer.style.left = "-600px";
   dim.style.display = "none";
   closeBtn.style.display = "none";
 });
